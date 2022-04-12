c46587074908d04274b6fc14e12da050dc7dad53
Alex Kim <312akim@gmail.com>
2022-04-11 21:02:08 -0700

    feat: add BaseTemplate component

---
+++ components/templates/base/BaseTemplate.mocks.ts
@@ -0,0 +1,15 @@
+// Mock props to test interface props
+import { IBaseTemplate } from "./BaseTemplate";
+
+const base: IBaseTemplate = {
+    sampleTextProp: 'Hello world!',
+}
+
+const alt: IBaseTemplate = {
+    sampleTextProp: 'Hello alt world!',
+}
+
+export const mockBaseTemplateProps = {
+    base,
+    alt,
+}
