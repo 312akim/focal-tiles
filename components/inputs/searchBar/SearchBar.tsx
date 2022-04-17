c406fc9f45466ead15b0eeaf7ceb299380146cd1
Alex Kim <312akim@gmail.com>
2022-04-16 20:59:53 -0700

    test: added searchBar user input test

--- components/inputs/searchBar/SearchBar.tsx
+++ components/inputs/searchBar/SearchBar.tsx
@@ -9,7 +9,7 @@ export interface ISearchBar {
 
 const SearchBar: React.FC<ISearchBar> = ({searchData}) => {
   const [show, setShow] = useState(true);
-  const [search, setSearch] = useState('');
+  const [userSearch, setUserSearch] = useState('');
 
   return (
     <div className='flex justify-self-start ml-10'>
@@ -18,16 +18,17 @@ const SearchBar: React.FC<ISearchBar> = ({searchData}) => {
       </label>
       <div className='my-auto'>
       <input
+        type='text'
         data-testid='headerSearchInput'
         id='headerSearchInput'
         placeholder='Search' 
-        className={`text-indigo my-auto text-left border-2 border-white pl-2 ${search === '' ? `w-32` : `w-48`} focus:w-48 focus:border-red transition-all`} 
-        onChange={(e) => setSearch(e.target.value)}
-        value={search}
+        className={`text-indigo my-auto text-left border-2 border-white pl-2 ${userSearch === '' ? `w-32` : `w-48`} focus:w-48 focus:border-red transition-all`} 
+        onChange={(e) => setUserSearch(e.target.value)}
+        value={userSearch}
         onBlur={() => setShow(false)}
         onFocus={() => setShow(true)}
       />
-      <div className={`${show && search !== '' ? '' : 'hidden'} text-indigo text-left pl-2 mt-1 border-2 border-indigo h-auto w-48 bg-white absolute`}>
+      <div className={`${show && userSearch !== '' ? '' : 'hidden'} text-indigo text-left pl-2 mt-1 border-2 border-indigo h-auto w-48 bg-white absolute`}>
         {
           searchData.map(({displayName}) => {
             return (
