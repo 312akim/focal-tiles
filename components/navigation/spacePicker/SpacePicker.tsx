0eb29ba092930802fe18afe0d75bd0a65f66d942
Alex Kim <312akim@gmail.com>
2022-04-19 11:09:51 -0700

    refactor: changed spacepicker onfocus to onclick, adjusted dropdown content padding

--- components/navigation/spacePicker/SpacePicker.tsx
+++ components/navigation/spacePicker/SpacePicker.tsx
@@ -12,12 +12,11 @@ const SpacePicker: React.FC<ISpacePicker> = ({ spaces }) => {
   const [hidden, setHidden] = useState(true);
 
   return (
-    <div className='flex-col mx-auto my-auto hover:cursor-pointer' tabIndex={0} onFocus={() => setHidden(false)} onBlur={() => setHidden(true)}>
+    <div className='flex-col mx-auto my-auto hover:cursor-pointer' tabIndex={0} onBlur={() => setHidden(true)} onClick={() => setHidden(!hidden)}>
       <div className='flex'>
         <h2 className='my-auto px-1 text-lg'>Space Picker</h2>
         <FontAwesomeIcon icon={faAngleDown} className='my-auto pl-1' size='lg'/>
       </div>
-      {/* The below is not rendering correctly */}
       <div>
         <DropDownMenu hidden={hidden} displayData={spaces} empty={false} width='w-32'/>
       </div>
--- components/utils/dropdown/DropDownMenu.tsx
+++ components/utils/dropdown/DropDownMenu.tsx
@@ -11,7 +11,7 @@ const DropDownMenu: React.FC<IDropDownMenu> = ({ hidden, empty, displayData, wid
     <div
       className={`${
         empty || hidden ? 'hidden' : ''
-      } rounded text-indigo text-left py-2 pl-2 mt-1 border-2 border-indigo h-auto ${width} bg-white absolute`}
+      } rounded text-indigo text-left p-2 mt-1 border-2 border-indigo h-auto ${width} bg-white absolute`}
     >
       {displayData.map(({ displayName }) => {
         return <div key={displayName}>{displayName}</div>;
