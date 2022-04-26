54808dd1bbb1b74f9919ebd1dab7a9037b7468eb
Alex Kim <312akim@gmail.com>
2022-04-26 15:45:45 -0700

    test: improved describes

--- components/tiles/tileWrapper/TileWrapper.test.tsx
+++ components/tiles/tileWrapper/TileWrapper.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import TileWrapper from './TileWrapper';
 
 describe('TileWrapper', () => {
-  it('renders text', () => {
+  it('renders text within a child element', () => {
     render(<TileWrapper><div>Tile Child</div></TileWrapper>);
 
     const text = screen.getByText(/Tile Child/i);
--- components/utils/modal/Modal.test.tsx
+++ components/utils/modal/Modal.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Modal from './Modal';
 
 describe('Modal', () => {
-  it('renders text', () => {
+  it('renders text within a child element', () => {
     render(<Modal><div>Child</div></Modal>);
 
     const text = screen.getByText(/Child/i);
