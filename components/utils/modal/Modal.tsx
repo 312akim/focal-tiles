171bc5a5a3bac821e7f8c5b1585dea34320d1868
Alex Kim <312akim@gmail.com>
2022-04-26 14:50:53 -0700

    feat: implemented modal in spacepicker, fixed vscode typescript settings

--- components/utils/modal/Modal.tsx
+++ components/utils/modal/Modal.tsx
@@ -7,7 +7,9 @@ export interface IModal {
 
 const Modal: React.FC<IModal> = ({ children }) => {
   return (
-    <div className={'flex justify-center fixed h-[100vh] w-[100%] opacity-50 bg-black inset-0'}>
+    <div 
+      className={'z-50 flex justify-center fixed h-[100vh] w-[100%] bg-primary-700 inset-0'} 
+      onClick={(e) => e.stopPropagation()}>
       <div className='mt-20'>
         {children}
       </div>
