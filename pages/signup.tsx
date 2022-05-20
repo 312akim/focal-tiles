38f5ae7a6295880d16aa3c31f31424d52fbececd
Alex Kim <312akim@gmail.com>
2022-05-19 18:47:21 -0700

    feat: signup page error setting and types implemented. form and textfield styling updates

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,9 +1,8 @@
-import React, { FormEvent, useState } from 'react';
+import React, { useState } from 'react';
 import UserPool from '../UserPool'
 import { CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js';
 import Modal from '../components/utils/modal/Modal';
 import TextField from '../components/inputs/textField/TextField';
-import { NextPage } from 'next';
 import Button from '../components/inputs/button/Button';
 
 const Signup = () => {
@@ -11,13 +10,14 @@ const Signup = () => {
     const [password, setPassword] = useState("");
     const [phoneNumber, setPhoneNumber] = useState("");
     const [nickname, setNickname] = useState("");
-    const [verifyCode, setVerifyCode] = useState("");
     const [signupError, setSignupError] = useState("");
-    const [modal, setModal] = useState(true);
-    const [formError, setFormError] = useState("");
+    const [verifyCode, setVerifyCode] = useState("");
+    const [codeError, setCodeError] = useState("");
+    const [modal, setModal] = useState(false);
 
     const onSignupSubmit = async (event: React.SyntheticEvent) => {
         event.preventDefault();
+
         try {
             setSignupError("");
             
@@ -30,57 +30,66 @@ const Signup = () => {
                 Name: "phone_number",
                 Value: `+1${phoneNumber}`,
             });
-        const attributeNickname = new CognitoUserAttribute({
-            Name: "nickname",
-            Value: nickname,
-        });
-        
-        attributeList.push(attributeEmail)
-        attributeList.push(attributePhoneNumber)
-        attributeList.push(attributeNickname)
+            const attributeNickname = new CognitoUserAttribute({
+                Name: "nickname",
+                Value: nickname,
+            });
+            
+            attributeList.push(attributeEmail)
+            attributeList.push(attributePhoneNumber)
+            attributeList.push(attributeNickname)
 
-        UserPool.signUp(email, password, attributeList, [], (err, result) => {
-            if (err) {
-                //set error
-                setSignupError(err.message)
-            }
-            console.log(result);
-            if (result) {
-                console.log(result.user);
-                setModal(true);
-            }
-        })
+            return await UserPool.signUp(email, password, attributeList, [], (err, result) => {
+                console.log(JSON.stringify(err))
+                if (err) {
+                    return setSignupError(err.message || JSON.stringify(err));
+                }
+                if (result) {
+                    return setModal(true);
+                }
+            })
+
         } catch (err) {
-            console.log(err);
+            if (err instanceof Error) {
+                return setSignupError(err.message || JSON.stringify(err));
+            }
         }
     }
 
-    const onSignupCodeSubmit = (event: React.SyntheticEvent) => {
+    const onSignupCodeSubmit = async (event: React.SyntheticEvent) => {
         event.preventDefault();
-        setFormError('')
-        if (!formError) {
-            return setFormError('Field must not be empty')
-        }
-
-        const userData = {
-            Username: email,
-            Pool: UserPool
-        }
-
-        const cognitoUser = new CognitoUser(userData);
-        cognitoUser.confirmRegistration('123456', true, (err, result) => {
-            if (err) {
-                console.log(err)
+        try {
+            setCodeError('');
+            if (!verifyCode) {
+                return setCodeError('Field must not be empty')
             }
-            console.log(result)
-        })
+            
+            const userData = {
+                Username: email,
+                Pool: UserPool
+            }
+            
+            const cognitoUser = new CognitoUser(userData);
+            return await cognitoUser.confirmRegistration(verifyCode, true, (err, result) => {
+                if (err instanceof Error) {
+                    return setCodeError(err.message || JSON.stringify(err));
+                }
+                if (result) {
+                    return setModal(false)
+                }
+            })
+        } catch (err) {
+            if (err instanceof Error) {
+                return setCodeError(err.message || JSON.stringify(err));
+            }
+        }
     }
 
     return (
-        <div className='flex flex-col justify-center'>
-            <h2>Signup Form</h2>
-            <form onSubmit={onSignupSubmit} className="flex flex-col items-center w-auto m-auto p-9 border-black border-2">
-                <div>{signupError}</div>
+        <div className='flex flex-col justify-center bg-primary-700 h-[100vh] p-3'>
+            <form onSubmit={onSignupSubmit} className="flex flex-col items-center w-auto min-h-[472px] mt-20 sm:mt-24 m-auto p-9 bg-primary-400 border-white border-2 shadow-xl">
+                <h1 className='text-xl'>Create Account</h1>
+                <div className='text-highlight my-3'>{signupError}</div>
                 <TextField id='loginEmail' label='Email' value={email} setValue={setEmail}/>
                 <TextField id='loginPhoneNo' label='Phone Number' value={phoneNumber} setValue={setPhoneNumber} />
                 <TextField id='nickname' label='Nickname' value={nickname} setValue={setNickname} />
@@ -88,19 +97,20 @@ const Signup = () => {
                 <Button type='submit'>Submit</Button>
             </form>
             {
-            modal ?
-            <div>
-                <Modal className='fixed top-24' bg={true}>
-                    <button onClick={() => setModal(false)}>Exit</button>
-                    <form onSubmit={onSignupCodeSubmit} className='flex flex-col items-center gap-9'>
-                        <h2 className='text-xl'>Code has been sent to your email!</h2>
-                        <TextField id='verifyCode' label='Verification Code' value={verifyCode} setValue={setVerifyCode} />
-                        <Button type='submit'>Submit</Button>
-                        <div>{formError}</div>
-                    </form>
-                </Modal>
-            </div>
-            : <div></div>         
+                modal ?
+                <div>
+                    <Modal className='fixed top-24' bg={true}>
+                        <button onClick={() => setModal(false)}>Exit</button>
+                        <form onSubmit={onSignupCodeSubmit} className='flex flex-col items-center gap-9'>
+                            <h1 className='text-xl'>Verify Email</h1>
+                            <h2>Check your email for the verification code</h2>
+                            <div>{codeError}</div>
+                            <TextField id='verifyCode' label='Verification Code' value={verifyCode} setValue={setVerifyCode} />
+                            <Button type='submit'>Submit</Button>
+                        </form>
+                    </Modal>
+                </div>
+                : <div></div>         
             }
         </div>
     );
